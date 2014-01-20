2607025e34665d8134e3c530d02c242bcba26f25
Konstantin Baierer <konstantin.baierer@gmail.com>
2014-01-20 13:45:06 +0100

    simple perl script to harvest data to fill cache

---
+++ dist/harvester.pl
@@ -0,0 +1,22 @@
+use Net::OAI::Harvester;
+use File::Slurp qw/read_file/;
+use strict;
+use warnings;
+use Data::Dumper;
+my $baseURL = 'http://141.20.126.232/dm2e-oai/oai';
+
+## create a harvester for the Library of Congress
+my $harvester = Net::OAI::Harvester->new( 
+    'baseURL' => $baseURL,
+);
+
+## list all the records in a repository
+my $records = $harvester->listAllRecords(
+    'metadataPrefix'    => 'oai_dc' 
+);
+while ( my $record = $records->next() ) {
+    my $header = $record->header();
+    my $metadata = $record->metadata();
+    print "identifier: ", $header->identifier(), "\n";
+    print "title: ", $metadata->title(), "\n";
+}
