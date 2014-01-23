9b07f01f14c5511d8413b5812d725e6f0d9f00bf
Konstantin Baierer <konstantin.baierer@gmail.com>
2014-01-23 22:11:23 +0100

    data.dm2e.eu domain working now

--- src/main/java/eu/dm2e/linkeddata/Config.java
+++ src/main/java/eu/dm2e/linkeddata/Config.java
@@ -1,6 +1,6 @@
 package eu.dm2e.linkeddata;
 
 public class Config {
-	public static final String	API_BASE	= "http://lelystad.informatik.uni-mannheim.de:3000/direct";
-//	public static final String	API_BASE	= "http://141.20.126.232/data";
+//	public static final String	API_BASE	= "http://lelystad.informatik.uni-mannheim.de:3000/direct";
+  	public static final String	API_BASE	= "http://data.dm2e.eu/data";
 }
