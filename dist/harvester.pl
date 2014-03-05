86e7976909bbdaff1d7a4a32028d126e6788de77
Konstantin Baierer <konstantin.baierer@gmail.com>
2014-03-05 09:57:44 +0100

    harvester.pl

--- dist/harvester.pl
+++ dist/harvester.pl
@@ -1,10 +1,13 @@
-use Net::OAI::Harvester;
-use File::Slurp qw/read_file/;
 use strict;
 use warnings;
 use Data::Dumper;
-my $baseURL = 'http://141.20.126.232/dm2e-oai/oai';
+use Net::OAI::Harvester;
+# use File::Slurp qw/read_file/;
+use Time::HiRes;
 
+my $baseURL = 'http://data.dm2e.eu/dm2e-oai/oai';
+
+print "Harvesting from $baseURL\n";
 ## create a harvester for the Library of Congress
 my $harvester = Net::OAI::Harvester->new( 
     'baseURL' => $baseURL,
@@ -14,9 +17,16 @@ my $harvester = Net::OAI::Harvester->new(
 my $records = $harvester->listAllRecords(
     'metadataPrefix'    => 'oai_dc' 
 );
+my $recordnumber = 0;
+my $start = Time::HiRes::gettimeofday();
 while ( my $record = $records->next() ) {
+    my $elapsed = (Time::HiRes::gettimeofday() - $start) * 1000;
     my $header = $record->header();
     my $metadata = $record->metadata();
     print "identifier: ", $header->identifier(), "\n";
     print "title: ", $metadata->title(), "\n";
+    print "Harvesting record # " . $recordnumber . "took $elapsed ms.\n";
+    $start = Time::HiRes::gettimeofday(); 
+    $recordnumber++;
 }
+print "Harvested $recordnumber records.";
