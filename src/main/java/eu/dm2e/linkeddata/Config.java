40dce909a81591a898876aa2ca04a6551befd69d
Konstantin Baierer <konstantin.baierer@gmail.com>
2014-03-07 14:27:52 +0100

    Drop the demo from feed.thepund.it

--- src/main/java/eu/dm2e/linkeddata/Config.java
+++ src/main/java/eu/dm2e/linkeddata/Config.java
@@ -6,6 +6,6 @@ public class Config {
 	public static final String API_BASE	= "http://data.dm2e.eu/data";
 	public static final String CACHE_SERIALIZATION = "RDF/XML";
 	public static final String CACHE_MEDIATYPE	= "application/rdf+xml";
-	public static final String PUNDIT_FMT_STRING = "http://demo.feed.thepund.it/?dm2e=%s&conf=dm2e.js";
+	public static final String PUNDIT_FMT_STRING = "http://feed.thepund.it/?dm2e=%s&conf=dm2e.js";
 	public static final String DEFAULT_THUMBNAIL = "http://openclipart.org/image/100px/svg_to_png/147409/1309149032.png";
 }
